2b2c1bee6207051237d2424d1da94ba298b4ccf9
cicero <ciceroamdrade2525@gmail.com>
2023-09-02 09:23:04 -0300

    removendo import

--- app/src/main/java/com/cicerodev/whatsappcomdi/ui/fragments/grupo/GrupoFragment.kt
+++ app/src/main/java/com/cicerodev/whatsappcomdi/ui/fragments/grupo/GrupoFragment.kt
@@ -2,7 +2,6 @@ package com.cicerodev.whatsappcomdi.ui.fragments.grupo
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
